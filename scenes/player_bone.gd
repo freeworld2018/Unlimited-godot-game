3c065a4a9ecc8c0bf9a2e5fab5eec91fa80f5a89
B <35457054+freeworld2018@users.noreply.github.com>
2025-06-12 13:19:00 +0800

    开始尝试技能类制作

--- scenes/player_bone.gd
+++ scenes/player_bone.gd
@@ -1,23 +1,61 @@
-extends Node2D
+extends CharacterBody2D
 
 var boxing_pre:bool = false
 var input_lock = false
 var boxing:int = 0
 var switch_walk = false
 
+#角色属性
+var hp:float = 300.0
+var mp:float = 300.0
+var gravity:float = 3000
+var jump_level : int = 0
+var current_item_id:int = -1
+var current_item:item
+var player_bag: Array[int] = []###已弃用
+var equiped_weapon
+var SPEED:float = 500.0          #移速
+var toward_right:bool = true     #朝向
+#var input_lock:bool =false       #输入锁定
+var can_use:bool = true         #使用锁定
 
 
+@onready  var animationplayer = $AnimationPlayer_hand
+@onready  var body = $"骨骼层"
+
+
+func _ready() -> void:
+	animationplayer.play("RESET")
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_left"):
-		self.position.x -= 1
-		self.switch_walk = true
-	if Input.is_action_pressed("ui_right"):
-		self.position.x += 1
-		self.switch_walk = true
-	if switch_walk:
-		$AnimationPlayer_foot.play("walk")
+	
+	#暂时的重力模拟
+	if is_on_floor():
+		jump_level = 0
+	else:
+		velocity.y += gravity * delta
+	
+	var direction2 = Input.get_axis("move_left", "move_right")
+	if direction2:
+		velocity.x = direction2 * SPEED
+		if direction2 < 0:
+			toward_right = false
+			
+		if direction2 > 0:
+			toward_right = true
+			
+		animationplayer.play("主角行走/walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animationplayer.play("RESET")
+
+	
+	if toward_right:
+		body.scale.x = 1
+	else:
+		body.scale.x = -1
+	move_and_slide()
 
 
 func _input(event: InputEvent) -> void:
